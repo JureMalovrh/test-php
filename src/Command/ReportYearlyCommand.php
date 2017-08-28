b0b7b066db5a7ea9ba64912d9e38a0ca864968f4
Jure Malovrh <malovrh.jure@gmail.com>
2017-08-28 23:06:44 +0200

    UPD add comments to the code

--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -33,38 +33,49 @@ class ReportYearlyCommand extends ContainerAwareCommand
         /* TODO: add prompt for a year */
         /* TODO: add prompt for name asc-desc */
 
+        // find min and max year so we know where to start and end
         $minMaxYear = $db->query('SELECT MIN(YEAR(date)) as min, MAX(YEAR(date)) as max from views')->fetchAll();
         $minYear = $minMaxYear[0]['min'];
         $maxYear = $minMaxYear[0]['max'];
 
+        // if anything is null (should not happen for max), then there is no data present
         if($minYear == NULL || $maxYear == NULL) {
             $io->text("There is no data in views table.");
             exit();
         } 
-        
+        /* 
+            go through all the years -> not really efficient, but somewhere needs to be done some loop, just because we have year + month not just one of them
+        */
+         /* TODO: try to optimize */
         for ($year=$minYear; $year <= $maxYear ; $year++) {
             $io->title($year);
             
+
             $views = $db->query("
-                SELECT v.profile_id, pr.profile_name AS name, MONTH(v.date) as month, YEAR(v.date), SUM(v.views) as views
+                SELECT v.profile_id, pr.profile_name AS name, MONTH(v.date) as month, SUM(v.views) as views
                 FROM views as v 
                 LEFT JOIN `profiles` as pr ON v.profile_id = pr.profile_id
                 WHERE YEAR(v.date) = {$year}
-                GROUP BY YEAR(v.date), MONTH(v.date), v.profile_id, pr.profile_name
+                GROUP BY MONTH(v.date), v.profile_id, pr.profile_name
                 ORDER BY pr.profile_name ASC, MONTH(v.date) ASC"
             )->fetchAll();
+
+            /* 
+                the data returned is going to be in a array, where the first person will be alphabetically first one, we just fill all the values we need (we put them in an array). If the name is different than the previous name, we put the line inside of our table data. 
+            */
 
             $name = NULL;
             $line = NULL;
             $dataForTable = array();
             foreach ($views as $data) {
+                // we got new name, so we should write line into table data
                 if($data["name"] != $name) {
                     $name = $data["name"];
                     
                     if(isset($line)) {
                         array_push($dataForTable, $line);
                     }
-
+                    // rewrite it and update name
                     $line = $this->emptyLine;
                     $line[0] = $name;
                     $line[$data["month"]] = $data["views"];
